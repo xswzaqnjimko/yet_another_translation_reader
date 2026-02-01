d093b7b6600132e8ca2a905d0143055a340497ac
xswzaqnjimko <35910992+xswzaqnjimko@users.noreply.github.com>
2026-01-31 23:58:21 -0800

    Delete yet_another_translation_reader directory

--- yet_another_translation_reader/background.js
+++
@@ -1,8 +0,0 @@
-// Background service worker - opens setup page when extension icon is clicked
-
-chrome.action.onClicked.addListener((tab) => {
-  // Open the setup page in a new tab
-  chrome.tabs.create({
-    url: chrome.runtime.getURL('popup/popup.html')
-  });
-});
